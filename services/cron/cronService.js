291a71ecbfc852094bb906bc10ca1c3fc96b4775
TatianaAll <tatiana.allery@orange.fr>
2026-06-15 12:24:53 +0200

    feat: update cron job to fetch random track and reset dailyDate

--- services/cron/cronService.js
+++ services/cron/cronService.js
@@ -1,20 +1,33 @@
 import cron from "node-cron";
 import prisma from "../../prismaClient.js";
+import { getRandomTrack } from "../spotify.js";
 
 // Define the hoir for the cron daily classic games
-cron.schedule("0 0 * * *", async () => {
+cron.schedule("23 12 * * *", async () => {
   await prisma.song.updateMany({
     where: { dailyDate: { not: null } },
     data: { dailyDate: null },
   });
 
-  const count = await prisma.song.count();
-  const skip = Math.floor(Math.random() * count);
-  const song = await prisma.song.findFirst({ skip });
+  const song = await getRandomTrack();
+  // const randomArtistName = song.artists[0].name;
+  console.log(song);
+  // let artistGenres = [];
+  // const artistsInfos = await getGenreOfATrack(randomArtistId);
+  // console.log(getGenreOfATrack(randomArtistName));
 
-  await prisma.song.update({
-    where: { id: song.id },
-    data: { dailyDate: new Date() },
+  await prisma.song.create({
+    data: {
+      idSpotify: song.id,
+      track: song.name,
+      artist: song.artists.map((a) => a.name).join(", "),
+      album: song.album.name,
+      year: song.album.release_date.slice(0, 4),
+      duration: Math.floor(song.duration_ms / 1000),
+      cover: song.album.images[2]?.url,
+      dailyDate: new Date(),
+      gameMode: "classic",
+    },
   });
 
   console.log(`Chanson du jour : ${song.track} - ${song.artist}`);
