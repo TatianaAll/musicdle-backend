3ca950237db0ab61d5c98d6322c7fc3df0aff1a8
eliott-colin <eliott.colin.pro@gmail.com>
2026-06-16 10:08:54 +0200

    fix: update cron schedule time for daily classic games

--- services/cron/cronService.js
+++ services/cron/cronService.js
@@ -3,7 +3,7 @@ import prisma from "../../prismaClient.js";
 import { getRandomTrack, getGenreOfATrack } from "../spotify.js";
 
 // Define the hoir for the cron daily classic games
-cron.schedule("0 10 * * *", async () => {
+cron.schedule("12 10 * * *", async () => {
   await prisma.song.updateMany({
     where: { dailyDate: { not: null } },
     data: { dailyDate: null },
