2ddbd43067946db57d928107a5cb8c4ce1a55e14
eliott-colin <eliott.colin.pro@gmail.com>
2026-06-17 15:25:27 +0200

    fix/backend

--- routes/game.js
+++ routes/game.js
@@ -4,7 +4,8 @@ const { postGuess, testRandom } = require("../controllers/gameControllers");
 const router = Router();
 
 router.post("/:gameId/guess", postGuess);
-router.get("/:gameId/guess/genre", getDailyGenre)
+// test de la fonction pour avoir une track random
+router.get('/test', testRandom);
 
 
 module.exports = router;
